a9efedf24b770b5b10a84ececc8ab70ff506bcac
Utkrist Karky <63005844+utkrist-karky@users.noreply.github.com>
2021-09-03 13:22:13 +0200

    Update Mover.cs

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -6,7 +6,7 @@ public class Mover : MonoBehaviour
 {
     [SerializeField]float xValue = 0; 
     [SerializeField]float yValue = 0;
-    [SerializeField]float zValue = -0.2f; 
+    [SerializeField]float zValue = -0.21f; 
 
 
     // Start is called before the first frame update
