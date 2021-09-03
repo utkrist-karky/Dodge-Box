0bea46442d75c5f75cea3d74c82ceef1cc646da4
Utkrist Karky <63005844+utkrist-karky@users.noreply.github.com>
2021-09-03 13:23:41 +0200

    Update Mover.cs

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -12,7 +12,7 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(0,0,0);
     }
 
     // Update is called once per frame
