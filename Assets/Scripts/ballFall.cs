dc8a223397c26295e8018cc45f5172a2b57e0707
Utkrist Karky <63005844+utkrist-karky@users.noreply.github.com>
2021-09-06 14:20:08 +0200

    Object falls

--- Assets/Scripts/ballFall.cs
+++ Assets/Scripts/ballFall.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class ballFall : MonoBehaviour
 {
+    MeshRenderer renderer; 
+    Rigidbody rigidBody; 
+    [SerializeField] float timeToWait = 5f; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        renderer = GetComponent<MeshRenderer>(); 
+        renderer.enabled = false; 
+        rigidBody.useGravity = false; 
     }
 
     // Update is called once per frame
+    
     void Update()
     {
-        if(Time.time == 3){
+        if(Time.time > timeToWait){
+            renderer.enabled = true; 
+            rigidBody.useGravity = true; 
             // GetComponent<MeshRenderer>().material.color = Color.red;
-            GetComponent<Rigidbody>().mass = 1; 
+            // GetComponent<Rigidbody>().mass = 1; 
         }
     }
 }
